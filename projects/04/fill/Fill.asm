91c80379c8556e55f47886ab2042d762b1e23f0e
jdkfx <amateurranch@gmail.com>
2021-06-27 16:08:48 +0900

    update

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,55 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// switch screen
+(LOOP)
+    @SCREEN
+    D = A
+    @ADDRESS
+    M = D   // M = SCREENのアドレス値
+
+    @KBD
+    D = M   // キーボードが押されていれば，そのキーボードの値
+
+    @BLACK  // キーボードが押されているので D の値が 0 より大きくなる
+    D;JGT   // if (D > 0) goto BLACK
+
+    @WHITE
+    0;JMP   // goto WHITE
+
+(BLACK)
+    @COLOR  // スクリーンに表示させたい色の値
+    M = -1  // -1(10) -> 1111 1111 1111 1111(2)
+
+    @DRAW
+    0;JMP
+
+(WHITE)
+    @COLOR  // スクリーンに表示させたい色の値
+    M = 0
+
+    @DRAW
+    0;JMP
+
+(DRAW)
+    @COLOR
+    D = M
+    @ADDRESS
+    A = M
+    M = D   // D で入力されたアドレス値の場所のスクリーンの色を変換させる
+    @ADDRESS
+    M = M + 1   // 次の色が変わっていないスクリーンのアドレス値をインクリメントで指定する
+
+    @8192   // (512 / 16) * 256 = 8K
+    D = A
+    @SCREEN
+    D = D + A
+    @ADDRESS
+    D = D - M
+
+    @DRAW
+    D;JGT   // if (8192 + SCREEN - ADDRESS > 0) goto DRAW
+
+    @LOOP
+    0;JMP
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,30 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+// R2 = R0 * R1
+    @R2
+    M = 0   // R2 = 0
+
+(LOOP)
+    @R1
+    D = M   // D = R1
+    @END
+    D;JEQ   // if (D = 0) goto END
+
+    @R0
+    D = M   // D = R0
+    @R2
+    M = M + D
+
+    @R1
+    D = M   // D = R1
+    @R1
+    M = M - 1
+
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
